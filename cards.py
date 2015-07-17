06e58efb6d49a45801353a514f14fbf59c0d51d1
jbaskauf <jessiebaskauf@jessies-air.att.net>
2015-07-17 13:15:46 -0500

    function to generate combinations

--- cards.py
+++ cards.py
@@ -1,3 +1,4 @@
+# This is a sample set of 12 cards that should have at least two solutions
 dealtCardAttribute=[
     ['purple','1','unfilled','squiggle'],
     ['purple','2','stripes','squiggle'],
@@ -13,3 +14,21 @@ dealtCardAttribute=[
     ['red','3','filled','diamond']
     ]
     
+nDealtCards=12
+
+# cardInCombination[[card0,card1,card2],[0,1,3],[0,1,4],... ]
+
+def generateCombinations(n):
+    allCombinations=[]
+    firstCard=0
+    while firstCard<n-2:
+        secondCard=firstCard+1
+        while secondCard<n-1:
+            thirdCard=secondCard+1
+            while thirdCard<n:
+                combination=[firstCard,secondCard,thirdCard]
+                allCombinations.append(combination)
+                thirdCard=thirdCard+1
+            secondCard=secondCard+1
+        firstCard=firstCard+1
+    return allCombinations
