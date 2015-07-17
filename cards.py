459bb19d6b935352e19c8465ac39d28067392e42
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2015-07-17 10:30:48 -0500

    generated test card set

---
+++ cards.py
@@ -0,0 +1,15 @@
+dealtCardAttribute=[
+    ['purple','1','unfilled','squiggle'],
+    ['purple','2','stripes','squiggle'],
+    ['green','1','stripes','squiggle'],
+    ['red','2','filled','oval'],
+    ['red','2','stripes','oval'],
+    ['purple','1','stripes','squiggle'],
+    ['purple','2','unfilled','oval'],
+    ['green','3','filled','squiggle'],
+    ['green','2','filled','squiggle'],
+    ['purple','3','unfilled','diamond'],
+    ['green','3','unfilled','diamond'],
+    ['red','3','filled','diamond']
+    ]
+    
