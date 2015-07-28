005cc8a6b8615ca3c858d60f1c061c66b532a47b
jbaskauf <jessiebaskauf@jessies-air.att.net>
2015-07-28 16:20:19 -0500

    added input routine to generate cards by clicking buttons
    established buttons for displaying results

--- cards.py
+++ cards.py
@@ -1,5 +1,19 @@
 # cardInCombination[[card0,card1,card2],[0,1,3],[0,1,4],... ]
 
+import tkinter
+from tkinter import *
+from tkinter import ttk
+import pdb
+
+# will be a list of which each item is a dealt card
+# each dealt card is itself a list of the values of the four attributes of that card
+dealtCardAttribute=[]
+
+# a list of values of the four attributes of the card being generated from the attribute buttons
+newCard=["","","",""]
+
+# this generates all possible combinations of 3 cards
+# returns a list of three-card combinations (where cards are represented by position numbers based on the order in which they were dealt)
 def generateCombinations(n):
     allCombinations=[]
     firstCard=0
@@ -15,6 +29,7 @@ def generateCombinations(n):
         firstCard=firstCard+1
     return allCombinations
 
+# checks one combination of cards to see if a particular attribute of the cards in the combination is the same for all three
 def checkCombinationAttributeForSame(attribute,combination,cards):
     firstCardIndex=combination[0]
     secondCardIndex=combination[1]
@@ -26,7 +41,8 @@ def checkCombinationAttributeForSame(attribute,combination,cards):
         return test
     else: test=False
     return test
-    
+
+# checks one combination of cards to see if a particular attribute is different
 def checkCombinationAttributeForDifferent(attribute,combination,cards):
     firstCardIndex=combination[0]
     secondCardIndex=combination[1]
@@ -42,13 +58,16 @@ def checkCombinationAttributeForDifferent(attribute,combination,cards):
     else: test=False
     return test
 
+# determines if the instances of a given attribute within a combinaton are all different or all the same
 def checkAttributeSuccess(attribute,combination,cards):
     attributeSame=checkCombinationAttributeForSame(attribute,combination,cards)
     attributeDifferent=checkCombinationAttributeForDifferent(attribute,combination,cards)
     if attributeSame | attributeDifferent:
         return True
     else: return False
-    
+
+# runs the same/different test for all four attributes for a given combination
+# if True, the combination is a set
 def checkCombination(combination,cards):
     attribute=0
     success=True
@@ -57,35 +76,113 @@ def checkCombination(combination,cards):
             success=False
         attribute=attribute+1
     return success
-        
-# Here is the start of the main script
-nDealtCards=12 #this should come from user input
 
-# This is a sample set of 12 cards that should have at least two solutions
-# This should also come from user input
-dealtCardAttribute=[
-    ['1','purple','unfilled','squiggle'],
-    ['2','purple','stripes','squiggle'],
-    ['1','green','stripes','squiggle'],
-    ['2','red','filled','oval'],
-    ['2','red','stripes','oval'],
-    ['1','purple','stripes','squiggle'],
-    ['2','purple','unfilled','oval'],
-    ['3','green','filled','squiggle'],
-    ['2','green','filled','squiggle'],
-    ['3','purple','unfilled','diamond'],
-    ['3','green','unfilled','diamond'],
-    ['3','red','filled','diamond']
-    ]
+# the function called to handle clicking any of the number buttons - puts clicked attribute into newCard list
+def defineNumber(string):
+    newCard[0]=string
+    number.set(string)
+
+# the function called to handle clicking any of the color buttons - puts clicked attribute into newCard list
+def defineColor(string):
+    newCard[1]=string
+    color.set(string)
+
+# the function called to handle clicking any of the fill buttons - puts clicked attribute into newCard list
+def defineFill(string):
+    newCard[2]=string
+    fill.set(string)
+
+# the function called to handle clicking any of the shape buttons - puts clicked attribute into newCard list
+def defineShape(string):
+    newCard[3]=string
+    shape.set(string)
+    
+# when the Add Card button is clicked, adds the new card (list) to the end of the list of dealt cards
+# clears display values for the new card
+def addCard(newCard,dealtCardAttribute):       
+    dealtCardAttribute.append(list(newCard))
+    color.set("")
+    shape.set("")
+    fill.set("")
+    number.set("")
+
+# this checks for sets and stores the card combinations in the set index list
+# steps through all generated combinations and checks whether each should be added to the list of sets
+def findSets(dealtCardAttribute):
+    allCombinations=generateCombinations(len(dealtCardAttribute))
+    nCombinations=len(allCombinations)
+    setIndices=[]
+    combinationIndex=0
+    while combinationIndex<nCombinations:
+        combination=allCombinations[combinationIndex]
+        if checkCombination(combination,dealtCardAttribute):
+            setIndices.append(combination)
+        combinationIndex=combinationIndex+1
+    return setIndices
+
+# the function called to handle clicking the "Are there sets?" button
+def areThereSets(dealtCardAttribute):
+    if len(findSets(dealtCardAttribute))==0:
+        result="no"
+    else: result="yes"
+    answer.set(result)
+
+# the function called to handle clicking the "How many sets?" button
+def numberSets(dealtCardAttribute):
+    result=len(findSets(dealtCardAttribute))
+    answer.set(result)
+
+# the function called to handle clicking the "What are the sets?" button
+def displaySets(dealtCardAttribute):
+    result=findSets(dealtCardAttribute)
+    answer.set(result)
     
 
-allCombinations=generateCombinations(nDealtCards)
-nCombinations=len(allCombinations)
-setIndices=[]
-combinationIndex=0
-while combinationIndex<nCombinations:
-    combination=allCombinations[combinationIndex]
-    if checkCombination(combination,dealtCardAttribute):
-        setIndices.append(combination)
-    combinationIndex=combinationIndex+1
-    
+#this is the main script
+
+root = Tk()
+
+# this sets up the characteristics of the window
+root.title("Set Wizard")
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+# establishes grid of buttons and labels
+number = StringVar()
+ttk.Button(mainframe, text="1", command=lambda: defineNumber("1")).grid(column=3, row=3, sticky=W)
+ttk.Button(mainframe, text="2", command=lambda: defineNumber("2")).grid(column=4, row=3, sticky=W)
+ttk.Button(mainframe, text="3", command=lambda: defineNumber("3")).grid(column=5, row=3, sticky=W)
+ttk.Label(mainframe, textvariable=number).grid(column=6, row=3, sticky=(W, E))
+
+color = StringVar()
+ttk.Button(mainframe, text="purple", command=lambda: defineColor("purple")).grid(column=3, row=4, sticky=W)
+ttk.Button(mainframe, text="green", command=lambda: defineColor("green")).grid(column=4, row=4, sticky=W)
+ttk.Button(mainframe, text="red", command=lambda: defineColor("red")).grid(column=5, row=4, sticky=W)
+ttk.Label(mainframe, textvariable=color).grid(column=6, row=4, sticky=(W, E))
+
+fill = StringVar()
+ttk.Button(mainframe, text="unfilled", command=lambda: defineFill("unfilled")).grid(column=3, row=5, sticky=W)
+ttk.Button(mainframe, text="stripes", command=lambda: defineFill("stripes")).grid(column=4, row=5, sticky=W)
+ttk.Button(mainframe, text="filled", command=lambda: defineFill("filled")).grid(column=5, row=5, sticky=W)
+ttk.Label(mainframe, textvariable=fill).grid(column=6, row=5, sticky=(W, E))
+
+shape = StringVar()
+ttk.Button(mainframe, text="squiggle", command=lambda: defineShape("squiggle")).grid(column=3, row=6, sticky=W)
+ttk.Button(mainframe, text="oval", command=lambda: defineShape("oval")).grid(column=4, row=6, sticky=W)
+ttk.Button(mainframe, text="diamond", command=lambda: defineShape("diamond")).grid(column=5, row=6, sticky=W)
+ttk.Label(mainframe, textvariable=shape).grid(column=6, row=6, sticky=(W, E))
+
+# this is the grid of test buttons
+ttk.Button(mainframe, text="Add Card", command=lambda: addCard(newCard, dealtCardAttribute)).grid(column=6, row=7, sticky=W)
+
+answer=StringVar()
+ttk.Button(mainframe, text="Are there sets?", command=lambda: areThereSets(dealtCardAttribute)).grid(column=3, row=8, sticky=W)
+ttk.Button(mainframe, text="How many sets?", command=lambda: numberSets(dealtCardAttribute)).grid(column=4, row=8, sticky=W)
+ttk.Button(mainframe, text="What are the sets?", command=lambda: displaySets(dealtCardAttribute)).grid(column=5, row=8, sticky=W)
+
+# this is the answer row
+ttk.Label(mainframe, textvariable=answer).grid(column=3, row=9, sticky=(W,E))
+
+mainloop()
